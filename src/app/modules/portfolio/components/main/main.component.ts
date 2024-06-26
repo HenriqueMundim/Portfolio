1dd1ec5226656cffd5e744233308a80d56b89d8c
Henrique Matos <henriquemundimmatos@gmail.com>
2024-06-26 14:23:56 -0300

    feat: card component created Create a componetent to projects and experiences card

--- src/app/modules/portfolio/components/main/main.component.ts
+++ src/app/modules/portfolio/components/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CurriculumComponent } from '../curriculum/curriculum.component';
+import { ProjectsComponent } from '../projects/projects.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CurriculumComponent],
+  imports: [CurriculumComponent, ProjectsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
