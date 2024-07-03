085b973d6341fc9d4625d381098ee262930533e8
Henrique Matos <henriquemundimmatos@gmail.com>
2024-07-02 21:15:22 -0300

    feat: Backend tech slider

--- src/app/modules/portfolio/components/tech-slider/tech-slider.component.ts
+++ src/app/modules/portfolio/components/tech-slider/tech-slider.component.ts
@@ -1,23 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { CarouselComponent, CarouselControlComponent, CarouselIndicatorsComponent, CarouselInnerComponent, CarouselItemComponent, ThemeDirective } from '@coreui/angular';
 import { IcarouselItem } from '../../../../interface/IcarouselItem.interface';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tech-slider',
   standalone: true,
-  imports: [
-    CommonModule,
-    CarouselComponent,
-    CarouselControlComponent,
-    CarouselInnerComponent,
-    CarouselItemComponent,
-    CarouselIndicatorsComponent,
-    ThemeDirective,
-    RouterLink
-  ],
+  imports: [CommonModule],
   templateUrl: './tech-slider.component.html',
   styleUrl: './tech-slider.component.scss'
 })
