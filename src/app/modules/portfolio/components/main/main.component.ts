275207d154e33baec8bf6208a5e7b08fbdc131dd
Henrique Matos <henriquemundimmatos@gmail.com>
2024-07-03 13:25:40 -0300

    feat: About me info created

--- src/app/modules/portfolio/components/main/main.component.ts
+++ src/app/modules/portfolio/components/main/main.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CurriculumComponent } from '../curriculum/curriculum.component';
 import { ProjectsComponent } from '../projects/projects.component';
 import { TechnologiesComponent } from '@components/technologies/technologies.component';
+import { AboutComponent } from '@components/about/about.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CurriculumComponent, ProjectsComponent, TechnologiesComponent],
+  imports: [CurriculumComponent, ProjectsComponent, TechnologiesComponent, AboutComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
