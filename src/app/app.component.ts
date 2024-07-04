de548ce1e9efec8ab00e4019243fe00958396206
Henrique Matos <henriquemundimmatos@gmail.com>
2024-07-04 11:09:51 -0300

    Footer component
    * feat: Initial setup, component footer generated
    
    * feat: Footer created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './modules/portfolio/components/header/header.component';
 import { MainComponent } from './modules/portfolio/components/main/main.component';
+import { FooterComponent } from '@components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent],
+  imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent],
   template: 
   `
     <app-header></app-header>
     <app-main></app-main>
+    <app-footer></app-footer>
   `
 })
 export class AppComponent {
