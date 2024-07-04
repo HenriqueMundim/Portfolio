5792bafab682e128220095eba8558f171da514b0
Henrique Matos <henriquemundimmatos@gmail.com>
2024-07-04 09:29:11 -0300

    feat: About component created

--- src/app/modules/portfolio/components/about/about.component.ts
+++ src/app/modules/portfolio/components/about/about.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { ExperiencesCarouselComponent } from '@components/experiences-carousel/experiences-carousel.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [ExperiencesCarouselComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
+  public perfilUrl: string = '../../../../../assets/images/perfil.jfif'
 }
--- src/app/modules/portfolio/components/experiences-carousel/experiences-carousel.component.ts
+++ src/app/modules/portfolio/components/experiences-carousel/experiences-carousel.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild, signal } from '@angular/core';
+import { IexperienceInfo } from 'app/interface/IexperienceInfo';
+import KeenSlider, { KeenSliderInstance } from "keen-slider"
 
 @Component({
   selector: 'app-experiences-carousel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './experiences-carousel.component.html',
   styleUrl: './experiences-carousel.component.scss'
 })
 export class ExperiencesCarouselComponent {
+    @ViewChild("sliderRef") sliderRef!: ElementRef<HTMLElement>
 
+    public experiences = signal<Array<IexperienceInfo>>([
+      {
+        year: "2018",
+        description: "Started computer networking tech in CEFET-MG - Centro Federal de Tecnologia e Educação de Minas Gerais"
+      },
+      {
+        year: "2022",
+        description: "Complete computer networking tech in CEFET-MG"
+      },
+      {
+        year: "08/2023",
+        description: "Started an internship at Infinity School as a programming tutor and teacher"
+      },
+      {
+        year: "04/2024",
+        description: "Finish an internship in Infinity School"
+      },
+      {
+        year: "05/2024",
+        description: "Full Stack developer at Group FCamara"
+      },
+    ])
+
+
+    ngAfterViewInit() {
+      this.sliderRef = new KeenSlider(this.sliderRef.nativeElement, {
+        loop: true,
+        mode: 'free-snap',
+        slides: {
+          perView: 4,
+          spacing: 25,
+        },
+        breakpoints: {
+          '(max-width: 750px)': {
+            slides: {
+              perView: 1
+            }
+          }
+        }
+      })
+    }
 }
